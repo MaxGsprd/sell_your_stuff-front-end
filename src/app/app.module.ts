1be7ed2dda6f556939c21a3ac2f00c1790085207
MaxGsprd <max.gaspard@outlook.fr>
2023-02-02 15:56:01 +0100

    add posting-detail component base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostingCardComponent } from './components/posting/posting-card/posting-card/posting-card.component';
+import { PostingDetailComponent } from './components/posting/posting-detail/posting-detail/posting-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostingCardComponent
+    PostingCardComponent,
+    PostingDetailComponent
   ],
   imports: [
     BrowserModule,
