67c0e6f86de3b38df4f529862dcff911a59d5f9f
MaxGsprd <max.gaspard@outlook.fr>
2023-02-02 15:58:53 +0100

    add posting components detail and list base files

---
+++ src/app/components/posting/posting-detail/posting-detail/posting-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posting-detail',
+  templateUrl: './posting-detail.component.html',
+  styleUrls: ['./posting-detail.component.css']
+})
+export class PostingDetailComponent {
+
+}
---
+++ src/app/components/posting/posting-list/posting-list/posting-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-posting-list',
+  templateUrl: './posting-list.component.html',
+  styleUrls: ['./posting-list.component.css']
+})
+export class PostingListComponent {
+
+}
