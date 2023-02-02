854952a2fe0634562f0bc061fb2893b313c9026e
MaxGsprd <max.gaspard@outlook.fr>
2023-02-02 16:27:03 +0100

    add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PostingCardComponent } from './components/posting/posting-card/posting-card/posting-card.component';
 import { PostingDetailComponent } from './components/posting/posting-detail/posting-detail/posting-detail.component';
 import { PostingListComponent } from './components/posting/posting-list/posting-list/posting-list.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostingCardComponent,
     PostingDetailComponent,
-    PostingListComponent
+    PostingListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
