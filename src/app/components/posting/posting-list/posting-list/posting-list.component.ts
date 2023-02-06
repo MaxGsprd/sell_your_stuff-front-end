bc3d0cc37c00da13f0e4ff43242c4e08d751642d
MaxGsprd <max.gaspard@outlook.fr>
2023-02-06 18:14:42 +0100

    add posting cards html and modify navbar

--- src/app/components/posting/posting-list/posting-list/posting-list.component.ts
+++ src/app/components/posting/posting-list/posting-list/posting-list.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class PostingListComponent {
 
+  Items: Array<any> = [
+    {
+      "Id":1,
+      "Name":"Default product A",
+      "Category":"Clothe",
+      "Price":5
+    },
+    {
+      "Id":2,
+      "Name":"Default product B",
+      "Category":"Clothe",
+      "Price":70
+    },
+    {
+      "Id":3,
+      "Name":"Default product C",
+      "Category":"Clothe",
+      "Price":99
+    },
+    {
+      "Id":4,
+      "Name":"Default product D",
+      "Category":"Clothe",
+      "Price":8
+    },
+    {
+      "Id":5,
+      "Name":"Default product E",
+      "Category":"Clothe",
+      "Price":62
+    },
+    {
+      "Id":6,
+      "Name":"Default product F",
+      "Category":"Clothe",
+      "Price":102
+    }
+  ] 
 }
