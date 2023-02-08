9a78f7ae3f9e3e0209e88aefa832c3710f5f21f9
MaxGsprd <max.gaspard@outlook.fr>
2023-02-08 08:49:30 +0100

    add formsModule and post ad form base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { AdCardComponent } from './components/ads/ad-card/ad-card.component';
 import { AdsListComponent } from './components/ads/ads-list/ads-list.component';
 import { PostAdComponent } from './components/ads/post-ad/post-ad.component';
 import { AdDetailComponent } from './components/ads/ad-detail/ad-detail.component';
+import { UserLoginComponent } from './components/user/user-login/user-login.component';
+import { UserRegistrationComponent } from './components/user/user-registration/user-registration.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +20,15 @@ import { AdDetailComponent } from './components/ads/ad-detail/ad-detail.componen
     AdCardComponent,
     AdsListComponent,
     PostAdComponent,
-    AdDetailComponent
+    AdDetailComponent,
+    UserLoginComponent,
+    UserRegistrationComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
