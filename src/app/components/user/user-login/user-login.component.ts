499c674ff24eee38397b65615f2ecede5e6e16fb
MaxGsprd <max.gaspard@outlook.fr>
2023-02-09 11:29:50 +0100

    add login form

--- src/app/components/user/user-login/user-login.component.ts
+++ src/app/components/user/user-login/user-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-login',
@@ -6,8 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  userSubmitted!:boolean;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    
+    this.createLoginForm();
   }
+
+  createLoginForm(): void {
+    this.loginForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    });
+  }
+
+  onLogin(): void {
+    console.log(this.loginForm);
+    this.userSubmitted = true;
+    if (this.loginForm.valid) {
+      this.loginForm.reset();
+      this.userSubmitted = false;
+      let alert =  document.getElementById('form-valid-alert');
+      alert?.classList.remove('hidden');
+      alert?.classList.add('show');
+    } else {
+      let alert =  document.getElementById('form-invalid-alert');
+      alert?.classList.remove('hidden');
+      alert?.classList.add('show');
+    }
+  }
+
+    /**
+   * Getter methods for form controls
+   */
+    get name() { return this.loginForm.get('name'); }
+    get password() { return this.loginForm.get('password'); }
 }
