c1f15e095cd8ea558212f82d2eb8d3318cf23f8f
MaxGsprd <max.gaspard@outlook.fr>
2023-02-13 17:24:56 +0100

    add getCategories method

--- src/app/components/ads/ads-list/ads-list.component.ts
+++ src/app/components/ads/ads-list/ads-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AdService } from 'src/app/services/ad.service';
+import { CategoryService } from 'src/app/services/category.service';
 import { IAd } from '../IAd.interface';
+import { Category } from 'src/app/models/category';
 
 @Component({
   selector: 'app-ads-list',
@@ -11,20 +13,26 @@ export class AdsListComponent implements OnInit {
 
   public ads: Array<IAd> = [];
 
-  constructor(private adService: AdService) {}
+  constructor(private adService: AdService, private categoryService: CategoryService) {}
 
   ngOnInit() : void {
     this.adService.getAllAds().subscribe({
       next: (response) => {
         this.ads = response;
-        console.log(response)
+        // console.log(response)
         const newAd = JSON.parse(localStorage.getItem('newAd') as string);
-
         if (newAd) {
           this.ads = [newAd, ...this.ads];
         }
       },
       error: (error) => console.error(error)
     });
+
+    //TEST CATEGORY
+    this.categoryService.getCategories().subscribe({
+      next: (res: Array<Category>) => console.log(res),
+      error: (err) => console.log(err)
+    });
+
   }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  private url: string = "Category";
+
+  constructor(private http: HttpClient) { }
+
+  public getCategories(): Observable<Array<Category>> {
+    return this.http.get<Array<Category>>(`${environment.apiUrl}/${this.url}`);
+  }
 }
