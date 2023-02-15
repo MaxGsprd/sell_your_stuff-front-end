da1b573b47822a419c9bf0a27e27e84904741e7b
MaxGsprd <max.gaspard@outlook.fr>
2023-02-15 10:35:16 +0100

    start ad detail html and logic implementation

--- src/app/components/ads/ad-detail/ad-detail.component.ts
+++ src/app/components/ads/ad-detail/ad-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IAd } from 'src/app/models/IAd.interface';
 import { AdService } from 'src/app/services/ad.service';
 
 @Component({
@@ -9,15 +10,42 @@ import { AdService } from 'src/app/services/ad.service';
 })
 export class AdDetailComponent implements OnInit {
 
-  public adId!: number;
+  ad: any = {
+    id: undefined,
+    author : undefined,
+    title :undefined ,
+    description : undefined,
+    category : undefined,
+    price : undefined,
+    publicationDate : undefined,
+    condition : undefined,
+    location : undefined,
+    Image : undefined
+  };
 
   constructor(private route: ActivatedRoute, private adService: AdService) {}
 
   ngOnInit() {
-    this.adId = +this.route.snapshot.params['id'];
-    this.adService.getAd(this.adId).subscribe({
-      next: (res) => console.log(res),
+    const routeParams = this.route.snapshot.paramMap;
+    const adIdFromRoute = Number(routeParams.get('id'));
+
+    this.adService.getAd(adIdFromRoute).subscribe({
+      next: (res) => {
+        this.mapAdInterface(res);
+      },
       error: (err) => console.log(err)
     });
+  }
+
+  mapAdInterface(httpResponseObject: any) {
+    console.log(httpResponseObject)
+    this.ad.id = httpResponseObject.id;
+    this.ad.author = httpResponseObject.author;
+    this.ad.title = httpResponseObject.title;
+    this.ad.description = httpResponseObject.description;
+    this.ad.category = httpResponseObject.category;
+    this.ad.publicationDate = httpResponseObject.publicationDate;
+    this.ad.location = httpResponseObject.location;
+    this.ad.Image = httpResponseObject.Image;
   }
 }
