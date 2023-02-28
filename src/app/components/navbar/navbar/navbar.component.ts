3941412645de8b5d60f9ba2f9fc038f7946fd1f2
MaxGsprd <max.gaspard@outlook.fr>
2023-02-28 10:52:21 +0100

    add user name to navbar

--- src/app/components/navbar/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar/navbar.component.ts
@@ -1,28 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'src/app/services/token.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   loggedinUser!: string;
 
-  constructor (private tokenService: TokenService) {}
-  
-  userSignedIn() {
-    
+  constructor (private tokenService: TokenService, private userService: UserService) {}
+
+  ngOnInit(): void {
     if (this.tokenService.isLogged()) {
-      this.loggedinUser = "Gloubiste !";
-      return true;
-    } else {
-      return false;
+      this.userService.getLoggedInUserId().subscribe({
+        next: (res) =>  {
+          this.userService.getUser(parseInt(res)).subscribe(data => this.loggedinUser = data.name)
+        },
+        error: (err) => console.error(err)
+      });
     }
+  }
+
+  userSignedIn() {
+    return this.loggedinUser != null ? true : false;
   }
 
   signOut(): void {
     this.tokenService.clearToken();
   }
-
 }
