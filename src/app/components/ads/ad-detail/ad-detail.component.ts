97e250427f162423e11366de56fdaf56b004876a
MaxGsprd <max.gaspard@outlook.fr>
2023-03-01 19:48:57 +0100

    modify ad detail component

--- src/app/components/ads/ad-detail/ad-detail.component.ts
+++ src/app/components/ads/ad-detail/ad-detail.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { IAdResponseDto } from 'src/app/models/dtos/IAdResponseDto';
+import { IUserResponseDto } from 'src/app/models/dtos/IUserResponseDto';
+import { IMessage } from 'src/app/models/IMessage.interface';
 import { AdService } from 'src/app/services/ad.service';
+import { MessageService } from 'src/app/services/message.service';
+import { TokenService } from 'src/app/services/token.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-ad-detail',
@@ -10,14 +17,27 @@ import { AdService } from 'src/app/services/ad.service';
 })
 export class AdDetailComponent implements OnInit {
 
-  ad: IAdResponseDto = {} as IAdResponseDto;
-  mainImg = {id:1, src:"/assets/images/jazz1.jpg"};
-  images = [];
+  ad: IAdResponseDto | undefined;
+  messageForm!: FormGroup;
+  userSubmitted!:boolean;
+  currentUser: IUserResponseDto = {} as IUserResponseDto
 
-  constructor(private route: ActivatedRoute, private adService: AdService, private router: Router) {  }
+  constructor(private route: ActivatedRoute, 
+              private adService: AdService,
+              private toastr: ToastrService,
+              private tokenService: TokenService,
+              private userService: UserService,
+              private messageService: MessageService,
+              private router: Router) {  }
 
   ngOnInit(): void{
     this.getAd();
+    this.getCurrentUser();
+
+    this.messageForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      body: new FormControl('', [Validators.required])
+    });
   }
 
   getAd(): void {
@@ -31,8 +51,54 @@ export class AdDetailComponent implements OnInit {
     });
   }
 
-  changeMainImg(event: any) {
-    this.mainImg = event;
+  sendMessage() {
+    this.userSubmitted = true;
+    if (this.messageForm.valid) {
+      console.log("formValue", this.messageForm.value)
+      
+      let newMessage = this.messageFormToMessage(this.messageForm.value);
+      console.log("newMsg", newMessage);
+
+      this.messageService.postMessage(newMessage).subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => console.log(err)
+      });
+      
+      this.messageForm.reset();
+      this.userSubmitted = false;
+      this.toastr.success('Congratulations, your message has been sent.', 'Thank you !');
+    }
   }
 
+  messageFormToMessage(formValues: any): IMessage {
+    let message = {} as IMessage;
+    message.id = 0;
+    message.title = formValues.title;
+    message.body = formValues.body;
+    message.authorId = this.currentUser.id;
+    message.adId = this.ad?.id;
+    message.isRead = false;
+    message.date = new Date();
+    message.recipientId = this.ad?.user.id;
+    return message;
+  }
+
+  getCurrentUser() {
+    if (this.tokenService.isLogged()) {
+      this.userService.getLoggedInUserId().subscribe({
+        next: (res) =>  {
+          this.userService.getUser(parseInt(res)).subscribe( data => this.currentUser = data)
+        },
+        error: (err) => console.error(err)
+      });
+    }
+  }
+
+  /**
+   * Getter methods for form controls
+   */
+    get title() { return this.messageForm.get('title'); }
+    get body() { return this.messageForm.get('body'); }
 }
