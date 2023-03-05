4ce412b39201b1964f1e07473d6b9fb625b47797
MaxGsprd <max.gaspard@outlook.fr>
2023-03-05 16:35:02 +0100

    implement obervables unscubscription

--- src/app/components/user/user-edit/user-edit.component.ts
+++ src/app/components/user/user-edit/user-edit.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { takeUntil } from 'rxjs';
 import { IUserRequestDto } from 'src/app/models/dtos/IUserRequestDto';
 import { IUser } from 'src/app/models/IUser.interface';
 import { UserService } from 'src/app/services/user.service';
+import { Unsubscribe } from 'src/app/_helpers/_unscubscribe/unsubscribe';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.css']
 })
-export class UserEditComponent implements OnInit {
+export class UserEditComponent extends Unsubscribe implements OnInit, OnDestroy {
 
   user: IUser = {} as IUser;
   userEditionForm!: FormGroup;
@@ -21,21 +23,25 @@ export class UserEditComponent implements OnInit {
               private userService: UserService,
               private formBuilder: FormBuilder,
               private toastr: ToastrService, 
-              private router: Router) { }
+              private router: Router) {
+                super();
+               }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const userId = Number(routeParams.get('id'));
 
-    this.userService.getFullUser(userId).subscribe({
-      next: (res) => {
-        this.user = res;
-        this.userEditionForm.get('name')?.setValue(this.user.name);
-        this.userEditionForm.get('birthDate')?.setValue(this.user.birthDate);
-        this.userEditionForm.get('email')?.setValue(this.user.email);
-        this.userEditionForm.get('phone')?.setValue(this.user.phone);
-      },
-      error: (err) => console.log(err)
+    this.userService.getFullUser(userId)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: (res) => {
+          this.user = res;
+          this.userEditionForm.get('name')?.setValue(this.user.name);
+          this.userEditionForm.get('birthDate')?.setValue(this.user.birthDate);
+          this.userEditionForm.get('email')?.setValue(this.user.email);
+          this.userEditionForm.get('phone')?.setValue(this.user.phone);
+        },
+        error: (err) => console.log(err)
     });
 
     this.userEditionForm = this.formBuilder.group({
@@ -54,28 +60,30 @@ export class UserEditComponent implements OnInit {
     this.userSubmitted = true;
 
     if (this.userEditionForm.valid) {
+      console.log(this.userEditionForm.value)
       let newUser = this.userEditFormToDto(this.userEditionForm.value);
-      this.userService.updateUser(newUser).subscribe({
-        next: (res) => {
-          this.userEditionForm.reset();
-          this.userSubmitted = false;
-          this.router.navigate(['/myDashboard', res.id]);
-          this.toastr.success('Congratulations, your profile has been updated.');
-        },
-        error: (err) => {
-          console.log("error", err)
-          const userNameTakenErrorDiv = document.getElementById('username-exist');
-          userNameTakenErrorDiv?.classList.remove('hidden');
-          userNameTakenErrorDiv?.classList.add('show');
-        }
-      });
+      this.userService.updateUser(newUser)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe({
+          next: (res) => {
+            this.userEditionForm.reset();
+            this.userSubmitted = false;
+            this.router.navigate(['/myDashboard', res.id]);
+            this.toastr.success('Congratulations, your profile has been updated.');
+          },
+          error: () => {
+            const userNameTakenErrorDiv = document.getElementById('username-exist');
+            userNameTakenErrorDiv?.classList.remove('hidden');
+            userNameTakenErrorDiv?.classList.add('show');
+          }
+        });
     } else {
       alertDanger?.classList.remove('hidden');
       alertDanger?.classList.add('show');
     }
   }
   
-  userEditFormToDto(formValues: any): IUserRequestDto {
+  userEditFormToDto(formValues: Object): IUserRequestDto {
     let userDto = {} as IUserRequestDto;
     Object.assign(userDto, formValues);
     userDto.id = this.user.id;
