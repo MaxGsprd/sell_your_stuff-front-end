506f1e1444823cfd27d3ecc5c21cdbc24d75c141
MaxGsprd <max.gaspard@outlook.fr>
2023-03-08 10:34:57 +0100

    fix environment import path

--- src/app/services/ad/ad.service.ts
+++ src/app/services/ad/ad.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { IAdResponseDto } from '../../models/dtos/IAdResponseDto';
 import { IAdRequestDto } from '../../models/dtos/IAdRequestDto';
 
