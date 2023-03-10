90c1ef0b63b7967555ffbe29cb7003a6901c5a15
MaxGsprd <max.gaspard@outlook.fr>
2023-03-10 09:14:13 +0100

    implement primary photo display on ad cards

--- src/app/components/ads/ad-card/ad-card.component.ts
+++ src/app/components/ads/ad-card/ad-card.component.ts
@@ -6,7 +6,18 @@ import { IAdResponseDto } from 'src/app/models/dtos/IAdResponseDto';
   templateUrl: './ad-card.component.html',
   styleUrls: ['./ad-card.component.css']
 })
-export class AdCardComponent {
+export class AdCardComponent implements OnInit {
   @Input() ad!: IAdResponseDto;
   @Input() adImagePreview!: Object;
+  primaryPhotoUrl: string = "assets/images/placeholder_img.png" ;
+
+  ngOnInit(): void {
+    if (this.ad.photos && this.ad.photos.length > 0) {
+      this.ad.photos.forEach( p => {
+        if (p.isPrimary) {
+          this.primaryPhotoUrl = p.imageUrl;
+        }
+      });
+    }
+  }
 }
