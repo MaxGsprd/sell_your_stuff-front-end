8c1def7ce1a1ded2ae6a9173af999204f8828824
Max <43386110+MaxGsprd@users.noreply.github.com>
2023-03-15 11:52:27 +0100

    correct ad component console errors

--- src/app/components/ads/ad-card/ad-card.component.ts
+++ src/app/components/ads/ad-card/ad-card.component.ts
@@ -12,7 +12,7 @@ export class AdCardComponent implements OnInit {
   primaryPhotoUrl: string = "assets/images/placeholder_img.png" ;
 
   ngOnInit(): void {
-    if (this.ad.photos && this.ad.photos.length > 0) {
+    if (this.ad?.photos && this.ad.photos.length > 0) {
       this.ad.photos.forEach( p => {
         if (p.isPrimary) {
           this.primaryPhotoUrl = p.imageUrl;
